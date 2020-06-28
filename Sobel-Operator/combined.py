ec590274b65ded3fab0a8a9ce6ea575847859a02
yuki678 <uk.12juni@gmail.com>
2020-06-28 10:16:37 +0100

    Complete Sobel Operator

---
+++ Sobel-Operator/combined.py
@@ -0,0 +1,34 @@
+import apply_sobel.abs_sobel_thresh
+import mag_dir.mag_thresh
+import dir_thres.dir_threshold
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pickle
+
+
+# Read in an image
+image = mpimg.imread('signs_vehicles_xygrad.png')
+
+# Choose a Sobel kernel size
+ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+# Apply each of the thresholding functions
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+ax1.imshow(image)
+ax1.set_title('Original Image', fontsize=20)
+ax2.imshow(combined, cmap='gray')
+ax2.set_title('Thresholded Grad. Dir.', fontsize=20)
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+plt.show()
+
